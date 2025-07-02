dc3c2d3355958269f2adfd8b98ff296f1796dd59
hzfhzf137 <hzfhzf137@gmail.com>
2025-07-03 01:36:38 +0500

    server.js
    port change from 5000 to 3000

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.use((err, req, res, next) => {
 });
 
 // ✅ MongoDB + Server Start
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000
 
 // Optional: Log unhandled rejections
 process.on('unhandledRejection', (reason, promise) => {
