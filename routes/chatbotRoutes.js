182b56b76d998f9b4f7d18bf9ef9a1b7054ccb3d
hzfhzf137 <hzfhzf137@gmail.com>
2025-07-17 14:06:51 +0500

    url
    changed

--- routes/chatbotRoutes.js
+++ routes/chatbotRoutes.js
@@ -60,7 +60,7 @@ router.post('/', verifyToken, async (req, res) => {
         headers: {
           "Authorization": `Bearer ${process.env.OPENROUTER_API_KEY}`,
           "Content-Type": "application/json",
-          "HTTP-Referer": "https://hzfhzf137.github.io/smart-edge", // Required by OpenRouter
+          "HTTP-Referer": "https://hzfhzf137.github.io", // Required by OpenRouter
           "X-Title": "SmartEdge Chatbot"
         }
       }
